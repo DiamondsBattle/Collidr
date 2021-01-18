ba367b507b48801ebcce88f0582207c13b17dea1
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-18 20:28:09 +0100

    Updated loading screen

--- prefabs/loading_screen.py
+++ prefabs/loading_screen.py
@@ -1,15 +1,17 @@
 from ursina import *
 
-class LoadingScreen(Entity):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
+class LoadingScreen:
+    def __init__(self):
         self.bg = Entity(
             model='quad',
             color=color.black,
+            parent=self,
         )
         self.bg.scale *= 400
-        self.text = Text('Loading...')
+        self.text = Text(
+            text='Loading...',
+            parent=self,
+        )
 
 
 if __name__ == '__main__':
