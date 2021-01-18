2bb14b4987588e667b0d634670578f8c92718c12
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-18 19:02:44 +0100

    Minor PEP-8 change

--- keybinds.py
+++ keybinds.py
@@ -1,12 +1,13 @@
-keybinds = {'player_forward': 'z', # Walk forward
-            'player_backward': 's', # Walk backward
-            'player_right': 'd', # Walk right
-            'player_left': 'q', # Walk left
-            'player_jump': 'space', # Jump
-            'player_interact': 'e', # Interact
-            'game_pause': 'p', # Pause the game (no menu)
-            'game_menu': 'escape', # Pause the game (with menu)
-            'ui_hide': 'u', # Hide the UI elements,
-            'weapon_use': 'left_mouse', # Use the current weapon (shoot, knife)
-            'weapon_aim': 'right_mouse', # Aim the current weapon (if scope is available)
-            }
+keybinds = {
+    'player_forward': 'z',  # Walk forward
+    'player_backward': 's',  # Walk backward
+    'player_right': 'd',  # Walk right
+    'player_left': 'q',  # Walk left
+    'player_jump': 'space',  # Jump
+    'player_interact': 'e',  # Interact
+    'game_pause': 'p',  # Pause the game (no menu)
+    'game_menu': 'escape',  # Pause the game (with menu)
+    'ui_hide': 'u',  # Hide the UI elements,
+    'weapon_use': 'left_mouse',  # Use the current weapon (shoot, knife)
+    'weapon_aim': 'right_mouse',  # Aim the current weapon (if scope is available)
+}
