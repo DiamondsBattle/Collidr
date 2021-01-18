d438a0354e0854c80612304c8ecef7e557417bd1
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-18 20:28:33 +0100

    Changed main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from ursina import *
 from direct.stdpy import thread
 from settings import video_settings, dev_settings
 
+
 def applyVideoSettings():
     if dev_settings['ursina_splash']:
         window.show_ursina_splash = True
@@ -10,13 +11,29 @@ def applyVideoSettings():
     if not window.fullscreen:
         window.size = video_settings['window_size']
 
-    window.vsync = video_settings['window.vsync']
+    window.vsync = video_settings['window_vsync']
 
 def loadEntities():
+    sky = Sky()
+    ground = Entity(
+        model='cube',
+        scale=Vec3(10, 1, 10),
+        collider='cube',
+        color=color.green,
+    )
+
+
+def showLoadingScreen():
     pass
 
 def start():
     applyVideoSettings()
+    showLoadingScreen()
+    try:
+        t = thread.start_new_thread(function=loadEntities)
+    except Exception:
+        print('unable to start a thread')
+
 
 def update():
     pass
@@ -24,6 +41,8 @@ def update():
 
 if __name__ == '__main__':
     app = Ursina()
+
+    loading = LoadingScreen()
 
     start()
 
