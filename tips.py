2d39bce1740cb5b01249ce0d1fb18520b97b7296
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-20 16:17:20 +0100

    Added tips

---
+++ tips.py
@@ -0,0 +1,7 @@
+from keybinds import keybinds
+
+tips = [
+    f'This is an exemple tip placeholder', # This should be the tips lenght
+    f'Open the minimap by pressing {keybinds["ui_minimap"]}',
+    f'You can adjust the graphics quality\nin the settings menu'
+]
