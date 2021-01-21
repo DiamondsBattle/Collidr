31332b2282687d4f8d5e614a1c68559ac0190ac2
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-21 19:00:39 +0100

    Fixed a tip width

--- tips.py
+++ tips.py
@@ -1,7 +1,8 @@
 from keybinds import keybinds
 
 tips = [
-    f'This is an exemple tip placeholder', # This should be the tips lenght
-    f'Open the minimap by pressing {keybinds["ui_minimap"]}',
-    f'You can adjust the graphics quality\nin the settings menu'
+    'This is an exemple tip placeholder', # This should be the tips lenght
+    f'Open the minimap by pressing {keybinds["ui_minimap"]}.',
+    'You can adjust the graphics quality\nin the settings menu.',
+    'This was made by the community ! \nJoin the project at :\ngithub.com/DiamondsBattle/Collidr'
 ]
