33d9e40f67f8658de63040e7448b73b0f8e0c46d
Louis Jarraud <diamondsbattle@tutanota.com>
2021-01-23 12:32:04 +0100

    Added Health Bar in health_bar.py

---
+++ prefabs/health_bar.py
@@ -0,0 +1,39 @@
+from ursina import *
+
+class HealthBar:
+    def __init__(self):
+        self.life = 100
+        self.bar = Entity(
+            model='quad',
+            color=color.cyan,
+        )
+
+        self.states = {
+            100: color.cyan,
+            80: color.lime,
+            50: color.yellow,
+            25: color.orange,
+            5: color.red,
+        }
+
+        self.changeLife()
+
+    def getColor(self):
+        for i in self.states:
+            print(i)
+            if i <= self.life:
+                return self.states[i]
+
+    def changeLife(self):
+        self.life -= 20
+        c = self.getColor()
+        self.bar.animate('color', c, duration=1)
+        invoke(self.changeLife, delay=2)
+
+
+if __name__ == '__main__':
+    app = Ursina()
+
+    a = HealthBar()
+
+    app.run()
