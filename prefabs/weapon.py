883c171e0ba3ed26f869ea515a18ba96db88b230
Louis Jarraud <diamondsbattle@tutanota.com>
2021-02-08 23:02:51 +0100

    Removed unnecessary variable referencing

--- prefabs/weapon.py
+++ prefabs/weapon.py
@@ -110,7 +110,7 @@ class Gun(Weapon):
 
         self.mag -= 1
 
-        bullet = Bullet(
+        Bullet(
             max_range=self.max_range,
             speed=self.sht_speed,
             rotation=self.rotation,
