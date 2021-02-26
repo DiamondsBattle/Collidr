d9996a6831c6c80ed6dfae0b4490e9473d77dfe7
Louis Jarraud <diamondsbattle@tutanota.com>
2021-02-26 13:12:26 +0100

    Made the game a Game() class inheriting from Ursina()

--- main.py
+++ main.py
@@ -7,67 +7,75 @@ from prefabs.controller import Controller
 from prefabs.weapon import Gun, BLA44, DEagle
 
 
-def applyVideoSettings():
-    window.show_ursina_splash = dev_settings['ursina_splash']
-    window.fullscreen = video_settings['window_fullscreen']
-    window.windowed_size = video_settings['window_size']
-    window.vsync = video_settings['window_vsync']
-    window.borderless = False
-    window.exit_button.input = None
+class Game(Ursina):
+    def __init__(self, spawn):
+        super().__init__()
 
-def applyAppSettings():
-    window.title = app_settings['title']
+        self.spawn = spawn
 
-def loadEntities():
-    global ld_scr
-    for i in range(10000): # dummy load
-        print(f'{i}/10000')
-    sky = Sky()
-    cube = Entity(
-        model='cube',
-        position=Vec3(0, 0, 10),
-        collider='box',
-        color=color.red,
-    )
-    ground = Entity(
-        model='map2',
-        scale=.01, # Vec3(10, 1, 100),
-        collider='mesh',
-        color=color.green,
-    )
-    # ground2 = duplicate(ground)
-    # ground2.y = -10
-    # ground2.scale = .1
-    player = Controller()
-    gun = BLA44(
-        ammo=666,
-        position=Vec3((player.x + 2), (player.y - .5), (player.z + 3.5)),
-    )
+        self.ld_scr = None
 
-    ld_scr.cleanDel()
+        self.mm = MainMenu(
+            multi=None,
+            sing=self.playSingleplayer,
+        )
 
-def start():
-    applyAppSettings()
-    applyVideoSettings()
+    @staticmethod
+    def applyVideoSettings():
+        window.show_ursina_splash = dev_settings['ursina_splash']
+        window.fullscreen = video_settings['window_fullscreen']
+        window.windowed_size = video_settings['window_size']
+        window.vsync = video_settings['window_vsync']
+        window.borderless = False
+        window.exit_button.input = None
 
-def playSingleplayer():
-    global ld_scr, mm
-    ld_scr = LoadingScreen()
-    mm.cleanDel()
+    @staticmethod
+    def applyAppSettings():
+        window.title = app_settings['title']
 
-    thread.start_new_thread(function=loadEntities, args='')
+    def loadEntities(self):
+        for i in range(10000): # dummy load
+            print(f'{i}/10000')
+        self.sky = Sky()
+        self.cube = Entity(
+            model='cube',
+            position=Vec3(0, 0, 10),
+            collider='box',
+            color=color.red,
+        )
+        self.ground = Entity(
+            model='map2',
+            scale=.01, # Vec3(10, 1, 100),
+            collider='mesh',
+            color=color.green,
+        )
+        self.player = Controller(
+            position=self.spawn
+        )
+        self.gun = BLA44(
+            ammo=666,
+            position=Vec3((self.player.x + 2), (self.player.y - .5), (self.player.z + 3.5)),
+        )
 
-def update():
-    pass
+        self.ld_scr.cleanDel()
+
+    def start(self):
+        self.applyAppSettings()
+        self.applyVideoSettings()
+
+    def playSingleplayer(self):
+        self.ld_scr = LoadingScreen()
+        self.mm.cleanDel()
+
+        thread.start_new_thread(function=self.loadEntities, args='')
+
+    def update(self):
+        pass
 
 
 if __name__ == '__main__':
-    app = Ursina()
+    game = Game(spawn=Vec3(0,0,0))
 
-    ld_scr = None
+    game.start()
 
-    mm = MainMenu(sing=playSingleplayer, multi=None)
-
-    start()
-
-    app.run()
+    game.run()
